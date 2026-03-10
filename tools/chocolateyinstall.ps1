917fd8eb6cc65fb508661b00a65d6c786b0571a5
Jerome Guibert <jguibert@gmail.com>
2026-03-10 02:27:10 +0100

    fix: define toolsDir variable in install script
    - Add missing toolsDir variable definition
    - Remove manual PATH installation (handled by Chocolatey)
    - Fix unzipLocation parameter error

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'agentctl'
 $version = '0.5.1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64 = "https://github.com/geronimo-iia/agentctl/releases/download/v$version/x86_64-pc-windows-msvc.zip"
 $checksum64 = 'aa7d9d4423330e42d5a26ebc6d981eec523218d5944154c7ef33acdc8f9e3336'
 
@@ -14,7 +15,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyZipPackage @packageArgs
-
-# Add to PATH
-$binPath = Join-Path $toolsDir 'agentctl.exe'
-Install-ChocolateyPath $toolsDir
